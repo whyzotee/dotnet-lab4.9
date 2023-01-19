8dccc4d1894110f0734b3d84e0e29df79cb09401
Z0TEExt <zenlektomyum@gmail.com>
2023-01-19 17:57:43 +0700

    update comment

--- dotNet-Lab4.9/Program.cs
+++ dotNet-Lab4.9/Program.cs
@@ -1,21 +1,30 @@
-﻿Console.Write("Enter number of loop : ");
+﻿// Input for looping time
+Console.Write("Enter number of loop : ");
 int looptime = Convert.ToInt32(Console.ReadLine());
+
+// Input for number use to find number nearest
 Console.Write("Enter number of n : ");
 long n = Convert.ToInt64(Console.ReadLine());
 
+// Defalut number nearest value
 long? number = null;
 
+// Defalut positive and negative numbers
 long numP = 9223372036854775807;
 long numM = -9223372036854775807;
 
+// Loop input check number nearest n
 for (int i = 1; i <= looptime; i++)
 {
+    // Input numbers
     Console.Write($"Enter number [{looptime - i + 1}] : ");
     long input = Convert.ToInt64(Console.ReadLine());
 
+    // Check if input is n?
     if (input == n)
         number = n;
 
+    // Check if number not equal n?
     if (number != n)
     {
         if (input - n >= 0 && input < numP)
@@ -26,10 +35,13 @@ for (int i = 1; i <= looptime; i++)
     }
 }
 
+// Check if number is's n?
 if (number != n)
 {
+    // Check if n is positive or negative?
     if (n >= 0)
     {
+        // Check if (positive variable or negative variable - n) > or < (positive variable or negative variable)
         if (numP - n < n - numM)
             number = numP;
         else if (numP - n > n - numM)
@@ -48,6 +60,8 @@ if (number != n)
     }
 }
 
+// Check if numbers nearest n is 2 numbers?
 string result = number == null ? ($"\nNuber nearest n({n}) is {numP} and {numM}") : ($"\nNuber nearest n({n}) is {number}");
 
+// Log result
 Console.WriteLine(result);
